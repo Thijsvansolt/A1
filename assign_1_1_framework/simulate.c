971e299eaa68614f685bbc66a432a071b2ae7738
Thijs van Solt <thijs.van.solt@student.uva.nl>
2022-11-13 15:19:08 +0100

    goede functie

--- assign_1_1_framework/simulate.c
+++ assign_1_1_framework/simulate.c
@@ -13,6 +13,7 @@
 
 
 /* Add any global variables you may need. */
+pthread_barrier_t barrier;
 
 struct all_threads {
     double *old;
@@ -24,6 +25,8 @@ struct all_threads {
     int t_max;
 };
 
+
+
 /* Add any functions you may need (like a worker) here. */
 
 // 1-dimensional wave equation function
@@ -31,8 +34,9 @@ void *wave_eq(void *all_info){
     struct all_threads *all = (struct all_threads *) all_info;
     for(int t = 1; t < all->t_max - 1; t++){
         for(int i = all->start; i < all->end - 1; i++){
-            all->new[i] = 2 * (all->current[i] - all->old[i]) + all->c*(all->old[i - 1] - (2 * (all->current[i] - all->current[i + 1])));
+            all->new[i] = 2 * all->current[i] - all->old[i] + all->c * (all->current[i - 1] - (2 * all->current[i] - all->current[i + 1]));
         }
+        pthread_barrier_wait(&barrier);
         double *temp = all->old;
         all->old = all->current;
         all->current = all->new;
@@ -61,8 +65,6 @@ int *make_ranges(int num_threads, int i_max){
     return ranges;
 }
 
-
-
 /*
  * Executes the entire simulation.
  *
@@ -78,40 +80,32 @@ int *make_ranges(int num_threads, int i_max){
 double *simulate(const int i_max, const int t_max, const int num_threads,
         double *old_array, double *current_array, double *next_array)
 {
+    pthread_barrier_init(&barrier, NULL, num_threads);
     int *ranges = make_ranges(num_threads, i_max);
-
-    struct all_threads *all_ranges = (struct all_threads*) malloc(num_threads * sizeof *all_ranges);
-    for (int i = 0; i < num_threads; i++) {
-        all_ranges[i].old = old_array;
-        all_ranges[i].current = current_array;
-        all_ranges[i].new = next_array;
-        all_ranges[i].c = 0.15;
-        all_ranges[i].start = ranges[2 * i];
-        all_ranges[i].end = ranges[2 * i + 1];
-        all_ranges[i].t_max = t_max;
-    }
-
-    /*
-     * After each timestep, you should swap the buffers around. Watch out none
-     * of the threads actually use the buffers at that time.
-     */
     int number = num_threads;
     pthread_t *thd;
     thd = (pthread_t*)malloc(sizeof(pthread_t)*number);
 
     for (int i = 0; i < number; i++){
-        pthread_create(&thd[i], NULL, wave_eq, &all_ranges[i]);
+        struct all_threads *all_info = (struct all_threads *)malloc(sizeof(struct all_threads));
+        all_info->old = old_array;
+        all_info->current = current_array;
+        all_info->new = next_array;
+        all_info->c = 0.15;
+        all_info->start = ranges[2 * i];
+        all_info->end = ranges[2 * i + 1];
+        all_info->t_max = t_max;
+        pthread_create(&thd[i], NULL, wave_eq, all_info);
     }
     for (int i = 0; i < number; i++){
         pthread_join(thd[i], NULL);
     }
-    // exit(0);
-    // printf("%f\n", arrays->c);
-
 
     /* You should return a pointer to the array with the final results. */
+    free(ranges);
+    free(thd);
     return current_array;
-    
+
 }
 
 
