8555bd60c3975e2312c03bc2a9664ea9aa5c09a6
Thijs van Solt <thijs.van.solt@student.uva.nl>
2022-11-16 14:47:52 +0100

    swaps

--- A2/assign2_framework/assign2_framework/assign2_1/simulate.cu
+++ A2/assign2_framework/assign2_framework/assign2_1/simulate.cu
@@ -43,8 +43,7 @@ static void checkCudaCall(cudaError_t result) {
 
 __global__ void wave_eq_Kernel(float *deviceA, float *deviceB, float *deviceC) {
     unsigned i = blockIdx.x * blockDim.x + threadIdx.x;
-    float temp = 2 * deviceB[i] - deviceA[i] + 0.15 * (deviceB[i - 1] - (2 * deviceB[i] - deviceB[i + 1]));
-    deviceC[i] = temp;
+    deviceC[i] = 2 * deviceB[i] - deviceA[i] + 0.15 * (deviceB[i - 1] - (2 * deviceB[i] - deviceB[i + 1]));
 }
 
 /* Function that will simulate the wave equation, parallelized using CUDA.
@@ -104,9 +103,14 @@ double *simulate(const long i_max, const long t_max, const long block_size,
         
 
         // Copy result back to host
-        checkCudaCall(cudaMemcpy(next_array, deviceA, i_max*sizeof(float), cudaMemcpyDeviceToHost));
-        checkCudaCall(cudaMemcpy(old_array, deviceB, i_max*sizeof(float), cudaMemcpyDeviceToHost));
-        checkCudaCall(cudaMemcpy(current_array, deviceC, i_max*sizeof(float), cudaMemcpyDeviceToHost));
+        checkCudaCall(cudaMemcpy(old_array, deviceA, i_max*sizeof(float), cudaMemcpyDeviceToHost));
+        checkCudaCall(cudaMemcpy(current_array, deviceB, i_max*sizeof(float), cudaMemcpyDeviceToHost));
+        checkCudaCall(cudaMemcpy(next_array, deviceC, i_max*sizeof(float), cudaMemcpyDeviceToHost));
+
+        float* temp = old_array;
+        old_array = current_array;
+        current_array = next_array;
+        next_array = temp;
     }
     // Cleanup GPU-side data
     checkCudaCall(cudaFree(deviceA));
