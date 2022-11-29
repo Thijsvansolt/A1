8d295b4d68f7eedbddef972bbd411255d55c4279
Thijs van Solt <thijs.van.solt@student.uva.nl>
2022-11-29 11:32:33 +0100

    script en comments

--- A3/assign_3_1_framework/assign3_3.c
+++ A3/assign_3_1_framework/assign3_3.c
@@ -1,13 +1,24 @@
+/*Name Thijs van Solt, Fedja Matti
+* Student ID: 13967681, 13953699
+* This file contains a broadcast function for a ring topology
+* A process can send a message using MPI to all other processes
+* To run this file use make broadcast and make runbroadcast
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
 #include <string.h>
 
+// this function is the module oparation
 int mod(int a, int b) {
     int r = a % b;
     return r < 0 ? r + b : r;
 }
 
+// This function broadcasts a message to all other processes
+// It stats with the root process and then sends the message to its left and right neighbors
+// These processes send the messages to  there own left and right neighbors.
 int MYMPI_Bcast(void *buffer, int count, MPI_Datatype datatype, int root, MPI_Comm comm) {
     int size;
     MPI_Comm_size(comm, &size);
@@ -25,25 +36,28 @@ int MYMPI_Bcast(void *buffer, int count, MPI_Datatype datatype, int root, MPI_Co
         MPI_Send(buffer, strlen(buffer) + 1, datatype, mod(root + 1, size) , 1, comm);
         MPI_Recv(buffer, strlen(buffer) + 1, datatype, mod(root + 1, size), 1, comm, MPI_STATUS_IGNORE);
         MPI_Send(buffer, strlen(buffer) + 1, datatype, mod(root - 1, size), 1, comm);
-        printf("final msg is %s\n", (char *)buffer);
+        printf("Broadcast msg is %s\n", (char *)buffer);
     }
 }
 
+// This function sets up the situation for the broadcast.
+// It creates a communicator and then calls the broadcast function.
+// It creates a message for the root process to broadcast.
+// and a filler message for the other processes.
 int main(int argc, char** argv) {
     MPI_Init(NULL, NULL);
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    char msg[4];
+    char msg[20];
 
     if (rank == 0) {
-        strcpy(msg, "Hoi");
+        strcpy(msg, "Hello world!");
         MYMPI_Bcast(msg, strlen(msg) + 1, MPI_CHAR, rank,  MPI_COMM_WORLD);
     } else {
-        strcpy(msg, "Hai");
+        strcpy(msg, "filler message");
         MYMPI_Bcast(msg, strlen(msg) + 1, MPI_CHAR, rank,  MPI_COMM_WORLD);
     }
-
 
     MPI_Finalize();
 }
---
+++ A3/assign_3_1_framework/script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Running 1 node with 8 processes"
+for i in 1000, 10000, 100000, 1000000, 10000000
+do
+    prun v -np 1 -8 -sge-script $PRUN_ETC/prun-openmpi ./assign3_1 $i 10000 >> 1_8_results.txt
+done
+
+echo "Running 8 node with 1 process"
+for i in 1000, 10000, 100000, 1000000, 10000000
+do
+    prun v -np 8 -1 -sge-script $PRUN_ETC/prun-openmpi ./assign3_1 $i 10000 >> 8_1_results.txt
+done
+
+echo "Running 8 node with 8 processes"
+for i in 1000, 10000, 100000, 1000000, 10000000
+do
+    prun v -np 8 -8 -sge-script $PRUN_ETC/prun-openmpi ./assign3_1 $i 10000 >> 8_8_results.txt
+done
